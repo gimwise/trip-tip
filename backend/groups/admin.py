e200de722b8e2cce130f557813d2f57441c8e19e
JaeSeo Yang <qud1251@gmail.com>
2022-07-20 18:03:41 +0900

    create model

--- backend/groups/admin.py
+++ backend/groups/admin.py
@@ -1,11 +1,33 @@
 from django.contrib import admin
 
-from .models import Group, Member
+from .models import *
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
+    ordering = ('group_id',)
     list_display = ('group_id', 'group_name', 'code', 'leader')
 
-# @admin.register(Member)
-# class MemberAdmin(admin.ModelAdmin):
-#     list_display = ('group_FK',)
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    ordering = ('-group_id',)
+    list_display = ('group_id', 'user_id',)
+
+@admin.register(Meeting)
+class MeetingAdmin(admin.ModelAdmin):
+    ordering = ('-group_id',)
+    list_display = ('meeting_id', 'meeting_name','group_id' ,'is_clear', 'create_dt')
+
+@admin.register(Receipt)
+class ReceiptAdmin(admin.ModelAdmin):
+    ordering = ('-meeting_id',)
+    list_display = ('receipt_name', 'paid_by', 'meeting_id', 'is_clear')
+
+@admin.register(Participant)
+class ParicipantAdmin(admin.ModelAdmin):
+    ordering = ('-receipt_id',)
+    list_display = ('user_id', 'receipt_id', 'is_clear')
+
+
+# admin.site.register(Meeting)
+# admin.site.register(Receipt)
+# admin.site.register(Participant)
--- backend/groups/models.py
+++ backend/groups/models.py
@@ -1,6 +1,19 @@
 from django.db import models
-import uuid
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+
+import random
+import string
+
+def get_group_code():
+    n = 30
+    res = ""
+    for i in range(n):
+        res += str(random.choice(string.ascii_uppercase))
+    return res
+
+CustomUser = settings.AUTH_USER_MODEL
 
 class Group(models.Model): # User : Group = 1 : N
     group_id = models.BigAutoField(
@@ -8,19 +21,66 @@ class Group(models.Model): # User : Group = 1 : N
         unique=True, 
         verbose_name="group_id"
     )
-    group_name = models.CharField(max_length=15, verbose_name="group_name", default="group")
-    code = models.UUIDField(
+    group_name = models.CharField(max_length=45, verbose_name="group_name", default="group")
+    code = models.CharField(
+        max_length=45,
         unique=True,
         editable=False,
-        default=uuid.uuid4,
+        default=get_group_code(),
         verbose_name="code",
     )
-    leader = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='leader_user', on_delete=models.CASCADE, null=False, db_column='leader')
-    member = models.ManyToManyField(settings.AUTH_USER_MODEL, through="Member", related_name="member_in_group")
+    leader = models.ForeignKey(CustomUser, related_name='leader_user', on_delete=models.CASCADE, null=True, db_column='leader')
 
     def __str__(self):
         return self.group_name
 
-class Member(models.Model): # Group : Member = 1 : N && User : Member = N : N
-    user_FK = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="group_member" , on_delete=models.CASCADE, db_column='user_FK')
-    group_FK = models.ForeignKey(Group, related_name="group_in", on_delete=models.CASCADE, db_column='group_FK')
+class Member(models.Model):
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, db_column='user_id',)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE, db_column='group_id',)
+
+class Meeting(models.Model):
+    meeting_id = models.BigAutoField(
+        primary_key=True, 
+        unique=True, 
+        verbose_name="meeting_id"
+    )
+    meeting_name = models.CharField(max_length=45, verbose_name="meeting_name", default="meeting")
+    create_dt = models.DateTimeField(default=timezone.now, blank=True, null=True)
+    is_clear = models.BooleanField(default=False)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE, db_column='group_id')
+
+    def __str__(self):
+        return self.meeting_name
+
+class Receipt(models.Model):
+    receipt_id = models.BigAutoField(
+        primary_key=True, 
+        unique=True, 
+        verbose_name="receipt_id"
+    )
+    receipt_name = models.CharField(max_length=45, verbose_name="receipt_name", default="receipt")
+    total = models.IntegerField(default=0,)
+    is_clear = models.BooleanField(default=False)
+    meeting_id = models.ForeignKey(Meeting, on_delete=models.CASCADE, db_column='meeting_id')
+    paid_by = models.ForeignKey(
+        CustomUser,
+        related_name='manager',
+        on_delete=models.CASCADE, 
+        db_column='paid_by',
+    )
+
+    def __str__(self):
+        return self.receipt_name
+    
+class Participant(models.Model):
+    participant_id = models.BigAutoField(
+        primary_key=True, 
+        unique=True, 
+        verbose_name="participant_id"
+    )
+    money = models.IntegerField(default=0)
+    is_clear = models.BooleanField(default=False)
+    receipt_id = models.ForeignKey(Receipt, on_delete=models.CASCADE, db_column='receipt_id')
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, db_column='user_id')
+    
+
