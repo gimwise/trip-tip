d6dc5934e8fff8e6c8e94b5103c6959632a65864
JaeSeo Yang <qud1251@gmail.com>
2022-07-26 10:58:52 +0900

    groups urls

--- backend/api/groups/urls.py
+++ backend/api/groups/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     GroupView,
     DetailGroupView,
+    CompletionGroupView,
     CreateGroupView,
     JoinGroupView,
 )
@@ -11,6 +12,7 @@ app_name = 'groups'
 urlpatterns = [
     path('', GroupView.as_view(), name="group-list"),
     path('<int:pk>/', DetailGroupView.as_view(), name="group-detail"),
+    path('<int:pk>/completion/', CompletionGroupView.as_view(), name="group-completion"),
     path('create/', CreateGroupView.as_view(), name="group-create"),
     path('join/', JoinGroupView.as_view(), name="group-join"),
 ]
